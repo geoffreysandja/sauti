9454a2756eb5d118cd144d84700d02bfde26b9a7
Geoffrey Sandja <geoffreysandja@gmail.com>
2018-08-30 13:43:38 +0530

    Adding Text-to-Speech Module

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { TextToSpeech } from '@ionic-native/text-to-speech';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public tts: TextToSpeech) {
 
+  }
+  say(name){
+    this.tts.speak(name)
+    .then(() => console.log('Success'))
+    .catch((reason: any) => console.log(reason));
   }
 
 }
