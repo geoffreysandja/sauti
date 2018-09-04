153e2d62939a6c25734d51863446769825083400
Geoffrey Sandja <geoffreysandja@gmail.com>
2018-09-04 14:55:27 +0530

    Adding sentence formation by click and repeat

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,11 +14,18 @@ export class HomePage {
   selected_subject:any;
   selected_verb:any;
   selected_complement:any;
+  selected_subject_voice_over:any;
+  selected_verb_voice_over:any;
+  selected_complement_voice_over:any;
   constructor(public navCtrl: NavController,public tts: TextToSpeech,public api:ApiProvider,public storage:Storage) {
     this.items=[];
     this.all_items=[];
     this.stack_item=[];
     this.selected_subject="assets/imgs/icons/select.png";
+    this.selected_subject_voice_over="I";
+    //default: verb (want)
+    this.selected_verb_voice_over="want";
+    this.selected_complement_voice_over="";
     this.selected_verb="assets/imgs/icons/select.png";
     this.selected_complement="assets/imgs/icons/select.png";
     this.api.getItemsByScreen('First').then((val)=>{
@@ -52,6 +59,22 @@ export class HomePage {
     });
    
   }
+  clearSubject(){
+    this.selected_subject="i";
+  }
+  clearVerb(){
+    this.selected_verb="assets/imgs/icons/select.png";
+  }
+  clearComplement(){
+    this.selected_complement="assets/imgs/icons/select.png";
+    this.selected_complement_voice_over="";
+  }
+  repeat(){
+    var word=this.selected_subject_voice_over+" "+this.selected_verb_voice_over+" "+this.selected_complement_voice_over;
+    this.tts.speak(word)
+    .then(() => console.log('Success'))
+    .catch((reason: any) => console.log(reason));
+  }
   populateGrid(item_name,screen_name,is_back){
     console.log('item name:'+item_name);
     for(var i=0;i<this.all_items.length;i++){
@@ -69,7 +92,9 @@ export class HomePage {
     var screen_name=this.stack_item.pop();
     this.populateGrid(screen_name,"",true);
   }
-  say(name,screen){
+  say(name,screen,id){
+    this.selected_complement="assets/imgs/images/"+id.toLowerCase()+".jpg";
+    this.selected_complement_voice_over=name;
     this.tts.speak(name)
     .then(() => console.log('Success'))
     .catch((reason: any) => console.log(reason));
